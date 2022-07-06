16bb84fb1b644adb9cfe2475ee0cf2a23d1107ee
cchinolla <cebastianchinolla@gmail.com>
2022-07-06 15:09:03 -0400

    added yellow detection

--- scripts/detect_yellow.py
+++ scripts/detect_yellow.py
@@ -30,70 +30,67 @@ def image_callback(camera_image):
         print(CvBridgeError)
 
     # get the dimensions of the image
-    height, width = cv_image.shape[0], cv_image.shape[1]
+    #height, width = cv_image.shape[0], cv_image.shape[1]
+
+    
+    # convert image to HLS
+    cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HLS)
 
     # crop the image
-    cv_image = cv_image[int(height / 2):, int(width / 3):]
+    cropped = cv_image[435:786, 340:500]
 
-    # filtered_image = color_filter(cv_image)
-    # cv2.imshow("FRAME", filtered_image)
+    #whitemask
+    # w_lower = np.array([np.round(0/2), np.round(0.75 * 255), np.round(0.00 * 255)])
+    # w_upper = np.array([np.round(360/2), np.round(1.00 * 255), np.round(0.30 * 255)])
+    # white_mask = cv2.inRange(cv_image, w_lower, w_upper)
 
+    #yellowmask
+    # y_lower = np.array([np.round(40/2), np.round(0.00 * 255), np.round(0.35 * 255)])
+    # y_upper = np.array([np.round(60/2), np.round(1.00 * 255), np.round(1.00 * 255)])
+    y_lower = np.uint8([RC.hue_l, RC.light_l, RC.sat_l])
+    y_upper = np.uint8([RC.hue_h, RC.light_h, RC.sat_h])
+    yellow_mask_uncropped = cv2.inRange(cv_image, y_lower, y_upper)
+    yellow_mask_cropped = cv2.inRange(cropped, y_lower, y_upper)
 
-    filtered_image = cv2.cvtColor(cv_image ,cv2.COLOR_BGR2GRAY)
+    #combine masks
+    # mask = cv2.bitwise_or(yellow_mask, white_mask)
+    # masked = cv2.bitwise_and(cv_image, cv_image, mask = mask)
 
-    filtered_image = cv2.GaussianBlur(filtered_image, (3, 3), 0)
+    contours, hier = cv2.findContours (yellow_mask_cropped, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    if not contours: # contours do not exists?
+        yellow_detected.data = False
+        yellow_detected_pub.publish(yellow_detected)
+        
 
-    canny_image = cv2.Canny(filtered_image, RC.canny_low, RC.canny_high)
+    # initialize the variables for computing the centroid and finding the largest contour
+    max_area = 0
+    max_contour = []
 
-    line_image = np.zeros_like(cv_image) # blank frame to draw the Hough lines
-    theta = RC.theta * (np.pi / 180)   # convert degrees to radians
+    if len(contours) != 0:
+        # find the largest contour by its area
+        max_contour = max(contours, key = cv2.contourArea)
+        max_area = cv2.contourArea(max_contour)
+    #draw the obtained contour lines(or the set of coordinates forming a line) on the original image
+    cv2.drawContours(cv_image, max_contour, -1, (0,0,255), 5) # BGR
 
-    # Hough line detection on the image image
-    hough_lines = cv2.HoughLinesP(canny_image, RC.rho, theta, RC.thresh, np.array([]), RC.min_length, RC.max_gap)
-    if hough_lines is not None:
-        for line in hough_lines:
-                for x1, y1, x2, y2 in line:
-                    cv2.line(line_image, (x1, y1), (x2, y2), (255, 255, 0), 5)
+    cv2.imshow("YELLOW_MASK_uncropped Window", yellow_mask_uncropped)
 
-    # draw the lines on the  image
-    hough_image = cv2.addWeighted(cv_image, 0.8, line_image, 1, 0)
+    # #If yellow blob big ---> say true
+    if max_area > 100:
+        cv2.drawContours(yellow_mask_cropped, max_contour, -1, (0,255,255), 5) # BGR
+        cv2.imshow("YELLOW_MASK DETECTED!!!", yellow_mask_cropped)
+        yellow_detected.data = True
+        yellow_detected_pub.publish(yellow_detected)
+        cv2.waitKey(3)
+    else:
+        yellow_detected.data = False
 
-    #
-    # # find contours in the binary (black and white) image
-    # contours, _ = cv2.findContours (image_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    #
-    # # initialize the variables for computing the centroid and finding the largest contour
-    # max_area = 0
-    # max_contour = []
-    #
-    # if len(contours) != 0:
-    #     # find the largest contour by its area
-    #     max_contour = max(contours, key = cv2.contourArea)
-    #     max_area = cv2.contourArea(max_contour)
-    # else:
-    #     yellow_detected.data = False
-    #     yellow_detected_pub.publish(yellow_detected) return
-    #
-    # try:
-    #     # draw the obtained contour lines (or the set of coordinates forming a line) on the original image
-    #     # https://learnopencv.com/find-center-of-blob-centroid-using-opencv-cpp-python/
-    #     cv2.drawContours(cv_image, max_contour, -1, (0, 0, 255), 5)
-    # except UnboundLocalError:
-    #     print("max contour not found")
-    #
-    # if max_area > 200:
-    #     yellow_detected.data = True
-    # else:
-    #     yellow_detected.data = False
-    #
-    # cv2.imshow("Yellow", cv_image)
+    yellow_detected_pub.publish(yellow_detected)
+    cv2.waitKey(3)
 
     # always publish false (WIP)
     yellow_detected.data = False
     yellow_detected_pub.publish(yellow_detected)
-
-    # cv2.imshow("HOUGH", hough_image)
-    cv2.waitKey(3)
 
 ################### helper functions ###################
 def color_filter(image):
